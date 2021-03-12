e53e30481af8ab43a43281477b4b33d9590f6d71
mustafatufan <mustafa@tufan.ee>
2021-03-12 21:59:57 +0300

    initial

---
+++ src/main/java/co/bilira/quote/QuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package co.bilira.quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/bilira/quote/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.bilira.quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
