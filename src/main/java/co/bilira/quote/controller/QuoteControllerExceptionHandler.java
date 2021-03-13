68a424212d190d8b338a76a60afd75d7ee5f5117
mustafatufan <mustafa@tufan.ee>
2021-03-14 00:27:39 +0300

    - added basic error logging

--- src/main/java/co/bilira/quote/controller/QuoteControllerExceptionHandler.java
+++ src/main/java/co/bilira/quote/controller/QuoteControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package co.bilira.quote.controller;
 
 import co.bilira.quote.service.InvalidAmountException;
 import co.bilira.quote.service.NoMarketException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,17 +17,20 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @ControllerAdvice
 public class QuoteControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private static final Logger log = LoggerFactory.getLogger(QuoteControllerExceptionHandler.class);
+
 	@ExceptionHandler({NoMarketException.class})
-	public ResponseEntity<String> throwNoMarketException(NoMarketException e) {
-		return error(NOT_FOUND, e);
+	public ResponseEntity<String> throwNoMarketException(NoMarketException ex) {
+		return error(NOT_FOUND, ex);
 	}
 
 	@ExceptionHandler({InvalidAmountException.class})
-	public ResponseEntity<String> throwInvalidAmountException(InvalidAmountException e) {
-		return error(BAD_REQUEST, e);
+	public ResponseEntity<String> throwInvalidAmountException(InvalidAmountException ex) {
+		return error(BAD_REQUEST, ex);
 	}
 
-	private ResponseEntity<String> error(HttpStatus status, Exception e) {
-		return ResponseEntity.status(status).body(e.getMessage());
+	private ResponseEntity<String> error(HttpStatus status, Exception ex) {
+		log.error(ex.getMessage());
+		return ResponseEntity.status(status).body(ex.getMessage());
 	}
 }
